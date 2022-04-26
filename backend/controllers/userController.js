47cd2224e74f8615436b7e4318476581203bd160
Nacho Martin <martin.nacho.31@gmail.com>
2022-04-26 12:40:44 +0200

    Verify account on login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,7 +27,37 @@ const createUser = async ({ body }, res) => {
   }
 };
 
+const logIn = async ({ body }, res) => {
+  const { email, password } = body;
+  const user = await User.findOne({ email });
+  let status;
+  let result;
+  if (!user) {
+    status = 404;
+    const error = new Error("User not found");
+    result = { message: error.message };
+  } else if (!user.confirmedAccount) {
+    status = 403;
+    const error = new Error("This account has not been verified");
+    result = { message: error.message };
+  } else if (await user.checkPassword(password)) {
+    const { _id, name, email: userEmail } = user;
+    status = 200;
+    result = ({
+      _id,
+      name,
+      email: userEmail
+    });
+  } else {
+    const error = new Error("Invalid password");
+    result = { message: error.message };
+    status = 403;
+  }
+  return res.status(status).json(result);
+};
+
 module.exports = {
   getUser,
-  createUser
+  createUser,
+  logIn
 };

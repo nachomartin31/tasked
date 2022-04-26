c481331bff4a8ab14a2d9dbce70fdee781d87ded
Nacho Martin <martin.nacho.31@gmail.com>
2022-04-26 12:41:14 +0200

    Add login method to router

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,9 +3,10 @@ const { Router } = require("express");
 const userRouter = Router();
 const userController = require("../controllers/userController");
 
-const { getUser, createUser } = userController;
+const { getUser, createUser, logIn } = userController;
 
 userRouter.get("/", getUser);
 userRouter.post("/", createUser);
+userRouter.post("/login", logIn);
 
 module.exports = userRouter;

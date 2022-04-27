06f650b357ebb06be740590c4431aa13fa1e26bf
Nacho Martin <martin.nacho.31@gmail.com>
2022-04-27 13:13:09 +0200

    Create project controller

---
+++ backend/controllers/projectController.js
@@ -0,0 +1,42 @@
+const getAllProjects = async (req, res) => {
+  res.status().json();
+};
+
+const createProject = async (req, res) => {
+  res.status().json();
+};
+
+const getOneProject = async (req, res) => {
+  res.status().json();
+};
+
+const updateProject = async (req, res) => {
+  res.status().json();
+};
+
+const deleteProject = async (req, res) => {
+  res.status().json();
+};
+
+const addCollaborator = async (req, res) => {
+  res.status().json();
+};
+
+const deleteCollaborator = async (req, res) => {
+  res.status().json();
+};
+
+const getTasks = async (req, res) => {
+  res.status().json();
+};
+
+module.exports = {
+  getAllProjects,
+  createProject,
+  getOneProject,
+  updateProject,
+  deleteProject,
+  addCollaborator,
+  deleteCollaborator,
+  getTasks
+};

6f2ac7457116a6f1160809fd98f8d148d57279ab
Nacho Martin <martin.nacho.31@gmail.com>
2022-04-27 15:44:22 +0200

    Add create project method

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,9 +1,19 @@
+const Project = require("../models/Project");
+
 const getAllProjects = async (req, res) => {
   res.status().json();
 };
 
-const createProject = async (req, res) => {
-  res.status().json();
+const createProject = async ({ user, body }, res) => {
+  const { _id: id } = user;
+  const project = { ...body, creator: id.toString() };
+  try {
+    const newProject = await Project.create(project);
+    res.status(201).json(newProject);
+  } catch (error) {
+    error.message = "Unable to create project";
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getOneProject = async (req, res) => {

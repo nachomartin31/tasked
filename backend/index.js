e57773d945cef3333079d3dcbd54788e88e924f6
Nacho Martin <martin.nacho.31@gmail.com>
2022-04-27 13:13:40 +0200

    Add project router

--- backend/index.js
+++ backend/index.js
@@ -16,4 +16,8 @@ const userRouter = require("./routes/userRouter");
 
 server.use("/users", userRouter);
 
+const projectRouter = require("./routes/projectRouter");
+
+server.use("/projects", projectRouter);
+
 server.listen(port, () => { debug(chalk.blue.bold(`Server is running on port ${port}`)); });
